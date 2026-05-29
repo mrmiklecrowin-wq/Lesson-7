9de2e14152aaee0957d37257ff395349ab0a3247
mrmiklecrowin-wq <mr.miklecrowin@gmail.com>
2026-05-29 13:51:57 +0300

    Add files via upload

---
+++ Lesson 7_2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+int swap(int &a,int &b)
+{
+int c = 0;	
+c = a;
+a = b;	
+b = c;	
+return 0;
+}
+
+int main(int argc, char** argv)
+{
+int a = 5, b = 8;
+	
+std::cout << "a = " << a << ", b = " << b << std::endl;
+	
+swap(a, b);
+	
+std::cout << "a = " << a << ", b = " << b << std::endl;
+	
+return 0;
+}	
