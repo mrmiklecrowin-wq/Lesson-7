a2026a2a8d4e9a7a1e7912749ac5ba5473090a0b
mrmiklecrowin-wq <mr.miklecrowin@gmail.com>
2026-05-29 14:59:17 +0300

    Update Lesson 7_2.cpp

--- Lesson 7_2.cpp
+++ Lesson 7_2.cpp
@@ -2,15 +2,18 @@
 
 int swap(int &a,int &b)
 {
+
 int c = 0;	
 c = a;
 a = b;	
 b = c;	
+
 return 0;
 }
 
 int main(int argc, char** argv)
 {
+
 int a = 5, b = 8;
 	
 std::cout << "a = " << a << ", b = " << b << std::endl;
